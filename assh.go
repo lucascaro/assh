8afbce57c29d18dc8d3585781a1678bdeb59d611
lucascaro <lucascaro@gmail.com>
2016-04-16 15:07:13 -0700

    Something to start with

---
+++ assh.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	"./filecache"
+)
+
+var fc = filecache.New("${HOME}/.asshcache")
+
+func main() {
+	fmt.Println("assh")
+	fmt.Print("\n\t ssh into auto scaling group\n\n")
+	flag.Usage = func() {
+		fmt.Printf("Usage:\n")
+		fmt.Println("\t assh [flags] <ASG Name>")
+		fmt.Println("\nFlags:")
+		flag.PrintDefaults()
+	}
+	sshKey := flag.String("i", "${HOME}/.ssh/fpc-rcloud-ecs.pem", "your ssh key")
+	user := flag.String("u", "ec2-user", "ssh user name")
+	flag.Parse()
+
+	asgName := flag.Arg(0)
+	fmt.Println("fetching IPs for asg: ", asgName)
+
+	ips := getIPAddresses(asgName)
+	fmt.Println("IP Addresses:", ips)
+	fmt.Println(*sshKey)
+
+	runSSH(ips, *user, *sshKey)
+}
+
+func runSSH(ips []string, user, sshKey string) {
+	cmd := "csshx"
+	args := []string{}
+	for _, ip := range ips {
+		args = append(args, user+"@"+ip)
+	}
+	args = append(args, "--ssh_args", "-i "+sshKey)
+	fmt.Println(cmd, strings.Join(args, " "))
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		fmt.Printf("ERROR: %s %s\n", err, out)
+		log.Fatal(err)
+	}
+}
+func getIPAddresses(asg string) []string {
+	ips := []string{}
+	ids := getInstanceIds(asg)
+	fmt.Println("Instance IDs:", ids)
+	ips = getInstanceIPs(ids)
+	return ips
+}
+
+func getInstanceIds(asg string) []string {
+	ids := []string{}
+	cmd := "aws"
+	args := []string{"autoscaling",
+		"describe-auto-scaling-groups",
+		"--auto-scaling-group-names",
+		asg,
+		"--query",
+		"AutoScalingGroups[0].Instances[]",
+		"--output",
+		"text",
+	}
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		fmt.Printf("ERROR: %s %s\n", err, out)
+		log.Fatal(err)
+	}
+
+	// Split lines
+	lines := strings.Split(string(out), "\n")
+	fmt.Printf("ASG DESCRIBE: %s\n", out)
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) > 0 {
+			ids = append(ids, fields[2])
+		}
+	}
+	return ids
+}
+
+func getInstanceIPs(instanceIds []string) []string {
+	var ips []string
+	var missingIds []string
+	for i, id := range instanceIds {
+		if cached, ok := fc.Get(id); ok == nil {
+			fmt.Println("Cached: ", id, cached)
+			ips = append(ips, cached.Value)
+		} else {
+			missingIds = append(missingIds, instanceIds[i])
+		}
+	}
+	instanceIds = missingIds
+	fmt.Println("Get ips for", instanceIds)
+	if len(instanceIds) > 0 {
+		cmd := "aws"
+		args := []string{
+			"ec2",
+			"describe-instances",
+			"--instance-ids",
+		}
+		args = append(args, instanceIds...)
+		args = append(args, "--query",
+			"Reservations[].Instances[].PrivateIpAddress",
+			"--output",
+			"text",
+		)
+		// fmt.Println(args)
+		out, err := exec.Command(cmd, args...).CombinedOutput()
+		if err != nil {
+			fmt.Printf("ERROR: %s %s\n", err, out)
+			log.Fatal(err)
+		}
+
+		ips = strings.Fields(string(out))
+
+		for i, ip := range ips {
+			fc.Set(instanceIds[i], ip)
+		}
+		fc.Save()
+		// fmt.Printf("INSTANCE DESCRIBE: %s\n", ips)
+	}
+
+	return ips
+}
---
+++ filecache/filecache.go
@@ -0,0 +1,89 @@
+package filecache
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// FileCache is the main struct for file based caches.
+type FileCache struct {
+	FileName string
+	Keys     map[string]CacheObject
+}
+
+// CacheObject is the representation of a value in the cache.
+type CacheObject struct {
+	Value   string
+	Expires uint64
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// New returns a new file cache from the specified file.
+func New(fileName string) *FileCache {
+	fileName = os.ExpandEnv(fileName)
+	cache := FileCache{
+		FileName: fileName,
+		Keys:     make(map[string]CacheObject),
+	}
+	cache.parse()
+	return &cache
+}
+
+// Get returns an element from the cache.
+func (f *FileCache) Get(key string) (CacheObject, error) {
+	obj := CacheObject{}
+	value, ok := f.Keys[key]
+	if !ok {
+		return obj, errors.New("Not found")
+	}
+	obj = value
+	return obj, nil
+}
+
+func (f *FileCache) parse() {
+	fd, err := os.Open(f.FileName)
+	if err != nil {
+		// File does not exist, nothing to parse.
+		return
+	}
+	defer fd.Close()
+
+	dat, err := ioutil.ReadAll(fd)
+	check(err)
+
+	stored := FileCache{
+		FileName: f.FileName,
+		Keys:     make(map[string]CacheObject),
+	}
+
+	json.Unmarshal(dat, &stored)
+	if err == nil {
+		f.Keys = stored.Keys
+		enc := json.NewEncoder(os.Stdout)
+		enc.Encode(f)
+	} else {
+		fmt.Println("ERROR Reading cache", err)
+	}
+}
+
+// Set a cache key
+func (f *FileCache) Set(key, value string) {
+	obj := CacheObject{value, 0}
+	f.Keys[key] = obj
+}
+
+// Save the file cache in the store file.
+func (f *FileCache) Save() {
+	j, _ := json.Marshal(f)
+
+	// fmt.Println("MARSHALLED: ", j)
+	ioutil.WriteFile(f.FileName, j, 0644)
+}
